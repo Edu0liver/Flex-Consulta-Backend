e72e6d33ecd6018651a4c7f0850933790d26934b
Eduardo <eduardooliveira256@gmail.com>
2024-03-30 15:15:02 -0300

    fix: ensuredAuthenticated in index routes

--- src/shared/routes/index.routes.ts
+++ src/shared/routes/index.routes.ts
@@ -2,8 +2,9 @@ import 'reflect-metadata';
 import { Router } from 'express';
 import { productsRoutes } from './products.routes';
 import { usersRoutes } from './users.routes';
+import { ensuredAuthenticated } from '../middleware/ensureAutheticated.middleware';
 
 export const router = Router();
 
 router.use('/products', productsRoutes);
-router.use('/users', usersRoutes);
+router.use('/users', ensuredAuthenticated, usersRoutes);
--- src/shared/routes/products.routes.ts
+++ src/shared/routes/products.routes.ts
@@ -3,7 +3,6 @@ import { GetProductsController } from '../../modules/products/services/getProduc
 import { CreateProductController } from '../../modules/products/services/createProduct/createProduct.controller';
 import { UpdateProductController } from 'src/modules/products/services/updateProduct/updateProduct.controller';
 import { DeleteProductController } from 'src/modules/products/services/deleteProduct/deleteProduct.controller';
-import { ensuredAuthenticated } from '../middleware/ensureAutheticated.middleware';
 
 export const productsRoutes = Router();
 
@@ -12,18 +11,10 @@ const createProductController = new CreateProductController();
 const updateProductController = new UpdateProductController();
 const deleteProductController = new DeleteProductController();
 
-productsRoutes.get('/', ensuredAuthenticated, getProductsController.handle);
+productsRoutes.get('/', getProductsController.handle);
 
-productsRoutes.post('/', ensuredAuthenticated, createProductController.handle);
+productsRoutes.post('/', createProductController.handle);
 
-productsRoutes.put(
-    '/:id',
-    ensuredAuthenticated,
-    updateProductController.handle,
-);
+productsRoutes.put('/:id', updateProductController.handle);
 
-productsRoutes.delete(
-    '/:id',
-    ensuredAuthenticated,
-    deleteProductController.handle,
-);
+productsRoutes.delete('/:id', deleteProductController.handle);
